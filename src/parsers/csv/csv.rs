bc639c711be6003f623ff957d5e3c518840ac328
Egorov Dmitry <dmitry.egorov@raiffeisen.ru>
2025-11-28 00:16:40 +0300

    CsvParser refactoring

--- src/parsers/csv/csv.rs
+++ src/parsers/csv/csv.rs
@@ -21,54 +21,55 @@ impl Parser for CsvParser {
 
         let mut lines = content.lines();
 
-        
-
         match lines.next() {
-            Some(first) if first == get_title_row() => {}
+            Some(first) if first == get_header_row() => {}
             _ => Err(CsvError::Title)?,
         }
 
-        let transactions: Result<Vec<Transaction>, CsvError> = lines
-            .enumerate()
-            .map(|(index, line)| {
-                let row: Vec<&str> = line.split(",").collect();
+        let mut transactions: Vec<Transaction> = vec![];
 
-                if row.len() != 8 {
-                    return Err(CsvError::Length { index });
-                }
+        for (index, line) in lines.enumerate() {
+            if line.is_empty() {
+                continue;
+            }
 
-                let parse_col_u64 = |i: usize, field: Field| {
-                    row[i].parse::<u64>().map_err(|_| CsvError::ParseField { index, field })
-                };
-                let parse_col_i64 = |i: usize, field: Field| {
-                    row[i].parse::<i64>().map_err(|_| CsvError::ParseField { index, field })
-                };
+            let row: Vec<&str> = line.split(",").collect();
 
-                Ok(Transaction {
-                    tx_id: parse_col_u64(0, Field::TxId)?,
-                    tx_type: row[1]
-                        .parse::<TxType>()
-                        .map_err(|_| CsvError::ParseField { index, field: Field::TxType })?,
-                    from_user_id: parse_col_u64(2, Field::FromUserId)?,
-                    to_user_id: parse_col_u64(3, Field::ToUserId)?,
-                    amount: parse_col_u64(4, Field::Amount)?,
-                    timestamp: parse_col_i64(5, Field::Timestamp)?,
-                    status: row[6]
-                        .parse::<TxStatus>()
-                        .map_err(|_| CsvError::ParseField { index, field: Field::Status })?,
-                    description: row[7].trim_matches('"').to_string(),
-                })
-            })
-            .collect();
+            if row.len() != 8 {
+                return Err(CsvError::Length { index });
+            }
 
-        transactions
+            let parse_col_u64 = |i: usize, field: Field| {
+                row[i].parse::<u64>().map_err(|_| CsvError::ParseField { index, field })
+            };
+            let parse_col_i64 = |i: usize, field: Field| {
+                row[i].parse::<i64>().map_err(|_| CsvError::ParseField { index, field })
+            };
+
+            transactions.push(Transaction {
+                tx_id: parse_col_u64(0, Field::TxId)?,
+                tx_type: row[1]
+                    .parse::<TxType>()
+                    .map_err(|_| CsvError::ParseField { index, field: Field::TxType })?,
+                from_user_id: parse_col_u64(2, Field::FromUserId)?,
+                to_user_id: parse_col_u64(3, Field::ToUserId)?,
+                amount: parse_col_u64(4, Field::Amount)?,
+                timestamp: parse_col_i64(5, Field::Timestamp)?,
+                status: row[6]
+                    .parse::<TxStatus>()
+                    .map_err(|_| CsvError::ParseField { index, field: Field::Status })?,
+                description: row[7].trim_matches('"').to_string(),
+            });
+        }
+
+        Ok(transactions)
     }
 
     fn write_to<W: Write>(
         transactions: &Vec<Transaction>,
         writer: &mut W
     ) -> Result<(), WriteError> {
-        writeln!(writer, "{}", get_title_row()).map_err(|_| WriteError::Write)?;
+        writeln!(writer, "{}", get_header_row()).map_err(|_| WriteError::Write)?;
         for t in transactions {
             let line = format!(
                 "{},{},{},{},{},{},{},\"{}\"",
@@ -88,7 +89,7 @@ impl Parser for CsvParser {
     }
 }
 
-fn get_title_row() -> String {
+fn get_header_row() -> String {
     let row = format!(
         "{}",
         Field::get_all()
@@ -96,4 +97,17 @@ fn get_title_row() -> String {
             .join(",")
     );
     row
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_header_row() {
+        assert_eq!(
+            get_header_row(),
+            "TX_ID,TX_TYPE,FROM_USER_ID,TO_USER_ID,AMOUNT,TIMESTAMP,STATUS,DESCRIPTION"
+        );
+    }
 }
